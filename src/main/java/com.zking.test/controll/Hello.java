fdeb985460a8f826e5e90eff59be4ba0f475c8bb
xxe28 <3108205631@qq.com>
2020-12-13 12:25:48 +0800

    Initial commit

---
+++ src/main/java/com.zking.test/controll/Hello.java
@@ -0,0 +1,14 @@
+package com.zking.test.controll;
+
+/**
+ * @author Xiu
+ * @site www.zking.com
+ * @company 公司
+ * @create  2020-12-12 21:45
+ */
+public class Hello {
+//    public
+public static void main(String[] args) {
+    System.out.println("hhh");
+}
+}
