584f256014b3c686666efff710a266a9a67a94df
xxe28 <3108205631@qq.com>
2020-12-14 11:01:55 +0800

    yttt

---
+++ src/main/java/com.zking.test/controll/Sb.java
@@ -0,0 +1,13 @@
+package controll;
+
+/**
+ * @author Xiu
+ * @site www.zking.com
+ * @company 公司
+ * @create  2020-12-14 10:59
+ */
+public class Sb {
+    public static void main(String[] args) {
+        System.out.println("hhh");
+    }
+}
